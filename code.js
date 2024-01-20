5caaa8f86ff519e79cb15dd2fad61a868068159a
Caden McFate <cadenmcfate@gmail.com>
2024-01-20 11:39:37 -0700

    completed

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
 function binarySearch(list, element) {
-    if (list.length > 0) {
-	var mid = Math.floor(list.length / 2);
-	if (element == list[mid]) {
-	    return mid;
-	} else if (element < list[mid]) {
-	    return binarySearch(list.slice(0,mid));
-	} else { return binarySearch(list.slice(mid,list.length)); }
-    }	
-    return -1;
+    if (list.length < 1) { return -1; }
+    if (list.length == 1) {
+	if (list[0] == element) { return 0; }
+	return -1;
+    }
+    var index = Math.floor(list.length / 2);
+    if (element == list[index]) {
+	if (element == list[index-1]) { return binarySearch(list.slice(0,index), element); } //finding the smallest index for a match.
+	else { return index; }
+    }
+    else if (element < list[index]) { return binarySearch(list.slice(0,index), element); }
+    else { return mid + binarySearch(list.slice(mid+1,list.length), element); }
 }
