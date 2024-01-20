702e83ded4a406ede51988d73cf0c97eb1800161
Caden McFate <cadenmcfate@gmail.com>
2024-01-20 10:23:52 -0700

    draft

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function binarySearch(list, element) {
+    if (list.length > 0) {
+	var mid = Math.floor(list.length / 2);
+	if (element == list[mid]) {
+	    return mid;
+	} else if (element < list[mid]) {
+	    return binarySearch(list.slice(0,mid));
+	} else { return binarySearch(list.slice(mid,list.length)); }
+    }	
     return -1;
 }
